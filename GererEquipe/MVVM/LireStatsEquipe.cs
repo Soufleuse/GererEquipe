f47c976fb4701292427d0f6be4aaa65bb9d72618
Steve Lemieux <lemsteinf@hotmail.com>
2023-11-03 15:58:55 -0400

    Progression maj stats equipe

--- GererEquipe/MVVM/LireStatsEquipe.cs
+++ GererEquipe/MVVM/LireStatsEquipe.cs
@@ -11,6 +11,12 @@ namespace GererEquipe.MVVM
         public LireStatsEquipe(StatsEquipeDto pStatsEquipe)
         {
             this.statsEquipe = pStatsEquipe;
+
+            // Voir le summary de la fonction
+            AllerChercherAnneeCourante();
+            statsEquipe.anneeStats = ConfigGlobale.Instance.AnneeCourante;
+
+            ListerEquipeRoutine();
         }
 
         public async void LireUneStatsEquipe(int idEquipe, short anneeStats)
@@ -30,8 +36,76 @@ namespace GererEquipe.MVVM
             }
         }
 
-        private string _messageErreur = string.Empty;
+        private bool _AfficherEntryNomEquipe = true;
+        public bool AfficherEntryNomEquipe
+        {
+            get { return _AfficherEntryNomEquipe; }
+            set
+            {
+                _AfficherEntryNomEquipe = value;
+                _AfficherPickerNomEquipe = !_AfficherEntryNomEquipe;
+                NotifierChangement("AfficherEntryNomEquipe");
+                NotifierChangement("AfficherPickerNomEquipe");
 
+                // Si _AfficherEntryNomEquipe est true, on affiche le bouton de création
+                // de la nouvelle stats.
+                AfficherBoutonNouvelleStats = _AfficherEntryNomEquipe;
+            }
+        }
+
+        private bool _AfficherPickerNomEquipe = false;
+        public bool AfficherPickerNomEquipe
+        {
+            get { return _AfficherPickerNomEquipe; }
+            set
+            {
+                _AfficherPickerNomEquipe = value;
+                _AfficherEntryNomEquipe = !_AfficherPickerNomEquipe;
+                NotifierChangement("AfficherEntryNomEquipe");
+                NotifierChangement("AfficherPickerNomEquipe");
+            }
+        }
+
+        private bool _AfficherBoutonNouvelleStats = true;
+        public bool AfficherBoutonNouvelleStats
+        {
+            get { return _AfficherBoutonNouvelleStats; }
+            set
+            {
+                _AfficherBoutonNouvelleStats = value;
+                NotifierChangement("AfficherBoutonNouvelleStats");
+            }
+        }
+
+        private List<EquipeDto> _listeEquipe = null;
+        public List<EquipeDto> listeEquipe { get { return _listeEquipe; } }
+
+        private EquipeDto _equipeSelectionnee = null;
+        public EquipeDto equipeSelectionnee
+        {
+            get { return _equipeSelectionnee; }
+            set
+            {
+                _equipeSelectionnee = value;
+                NotifierChangement("equipeSelectionnee");
+            }
+        }
+
+        private short _anneeStats = short.MinValue;
+        public short anneeStats
+        {
+            get { return _anneeStats; }
+            set
+            {
+                if(anneeStats != value)
+                {
+                    _anneeStats = value;
+                    NotifierChangement("anneeStats");
+                }
+            }
+        }
+
+        private string _messageErreur = string.Empty;
         public string messageErreur
         {
             get { return _messageErreur; }
@@ -41,6 +115,20 @@ namespace GererEquipe.MVVM
                 {
                     _messageErreur = value;
                     NotifierChangement("messageErreur");
+                }
+            }
+        }
+
+        private bool _estBtnSauvegarderEnabled = true;
+        public bool estBtnSauvegarderEnabled
+        {
+            get { return _estBtnSauvegarderEnabled; }
+            set
+            {
+                if (estBtnSauvegarderEnabled != value)
+                {
+                    _estBtnSauvegarderEnabled = value;
+                    NotifierChangement("estBtnSauvegarderEnabled");
                 }
             }
         }
@@ -58,6 +146,24 @@ namespace GererEquipe.MVVM
 
                 return _SauvegarderStatsEquipe;
             }
+        }
+
+        private async void ListerEquipeRoutine()
+        {
+            var monClientHttp = new EquipeServices();
+
+            var listeEquipe = await monClientHttp.ObtenirListeEquipeAsync();
+
+            foreach (var item in listeEquipe)
+            {
+                if (item.estDevenueEquipe.HasValue)
+                {
+                    item.nomEquipeVilleEstDevenueEquipe = await monClientHttp.ObtenirNomEquipeEstDevenu(item.estDevenueEquipe.Value);
+                }
+            }
+
+            _listeEquipe = listeEquipe;
+            NotifierChangement("listeEquipe");
         }
 
         private async void SauvegarderStatsEquipeRoutine(object objParametre)
@@ -95,9 +201,9 @@ namespace GererEquipe.MVVM
         private void InitialiserNouvelleStatsEquipeRoutine(object objParametre)
         {
             statsEquipe = new StatsEquipeDto();
-            // Voir le summary de la fonction
-            AllerChercherAnneeCourante();
-            statsEquipe.anneeStats = ConfigGlobale.Instance.AnneeCourante;
+
+            // AfficherPickerNomEquipe devrait se mettre à true automatique.
+            AfficherEntryNomEquipe = false;
         }
 
         /// <summary>
@@ -112,6 +218,8 @@ namespace GererEquipe.MVVM
 
                 ConfigGlobale.Instance.AnneeCourante = Convert.ToInt16(monAnneeHttp.First().valeur);
             }
+
+            anneeStats = ConfigGlobale.Instance.AnneeCourante;
         }
     }
 }
--- GererEquipe/Pages/UneStatsEquipe.xaml.cs
+++ GererEquipe/Pages/UneStatsEquipe.xaml.cs
@@ -10,4 +10,17 @@ public partial class UneStatsEquipe : ContentPage
 		InitializeComponent();
 		BindingContext = new LireStatsEquipe(statsEquipeSelectionnee);
 	}
+
+    private void pckEquipe_SelectedIndexChanged(object sender, EventArgs e)
+    {
+		var monContexte = (LireStatsEquipe)BindingContext;
+
+		monContexte.estBtnSauvegarderEnabled = false;
+		monContexte.statsEquipe.equipeId = 0;
+        if (monContexte.equipeSelectionnee != null)
+		{
+			monContexte.statsEquipe.equipeId = monContexte.equipeSelectionnee.id;
+			monContexte.estBtnSauvegarderEnabled = true;
+        }
+    }
 }
